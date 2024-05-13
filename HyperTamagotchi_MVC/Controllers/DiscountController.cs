54f81f461c5b022a11bfc4c77f6b50d4c11f1034
Tobias Skog <t_ott_e@hotmail.com>
2024-05-13 11:25:37 +0200

    Added ConversionHelper class and started on adding controllers for creating items, discounts etc

---
+++ HyperTamagotchi_MVC/Controllers/DiscountController.cs
@@ -0,0 +1,27 @@
+﻿using HyperTamagotchi_MVC.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace HyperTamagotchi_MVC.Controllers;
+[Authorize(Roles = "Admin")]
+public class DiscountController : Controller
+{
+    private readonly ApplicationDbContext _context;
+
+    public DiscountController(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<IActionResult> Index()
+    {
+        return View(await _context.ShoppingItems.ToListAsync());
+    }
+
+    public IActionResult AddDiscountToShoppingItems(List<int> selectedShoppingItems, float discountValue)
+    {
+        float discountPercentage = DiscountConversionHelper.ConvertFromUserInputToShoppingItem(discountValue);
+        return View();
+    }
+}
