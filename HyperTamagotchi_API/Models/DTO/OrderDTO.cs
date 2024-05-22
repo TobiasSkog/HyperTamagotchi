5c37602a91ffaea59d60b91f9e7da724d798fbb3
Tobias Skog <t_ott_e@hotmail.com>
2024-05-22 16:26:51 +0200

    a

--- HyperTamagotchi_API/Models/DTO/OrderDTO.cs
+++ HyperTamagotchi_API/Models/DTO/OrderDTO.cs
@@ -1,11 +1,31 @@
-﻿namespace HyperTamagotchi.Common.DTO
+﻿namespace HyperTamagotchi_API.Models.DTO;
+
+public class OrderDto
 {
-    public class OrderDto
-    {
-        public int OrderId { get; set; }
-        public DateTime OrderDate { get; set; }
-        public DateTime? ShippingDate { get; set; }
-        public DateTime? ExpectedDate { get; set; }
-        public List<ShoppingItemOrderDTO> Items { get; set; } = new();
-    }
+    public int OrderId { get; set; }
+
+    public CustomerDto Customer { get; set; }
+
+    public DateTime OrderDate { get; set; } = DateTime.Now;
+
+    public DateTime? ShippingDate { get; set; }
+
+    public DateTime? ExpectedDate { get; set; }
+
+    public ICollection<ShoppingItem> Items { get; set; } = [];
+}
+
+public class OrderDtoCheckout
+{
+    public int OrderId { get; set; }
+
+    public Customer Customer { get; set; }
+
+    public DateTime OrderDate { get; set; } = DateTime.Now;
+
+    public DateTime? ShippingDate { get; set; }
+
+    public DateTime? ExpectedDate { get; set; }
+
+    public List<ShoppingItemOrderDTO> Items { get; set; } = [];
 }
